dc81173861966c6785a6989d4e0e1570bf3d0a9a
varun bansal <varunbansal@varuns-MacBook-Pro.local>
2025-11-16 23:32:55 +0530

    zigzag

---
+++ src/Zigzag.java
@@ -0,0 +1,79 @@
+//The string "PAYPALISHIRING" is written in a zigzag pattern on a given number of rows like this: (you may want to display this pattern in a fixed font for better legibility)
+//
+//P   A   H   N
+//A P L S I I G
+//Y   I   R
+//And then read line by line: "PAHNAPLSIIGYIR"
+//
+//Write the code that will take a string and make this conversion given a number of rows:
+//
+//string convert(string s, int numRows);
+//
+//
+//Example 1:
+//
+//Input: s = "PAYPALISHIRING", numRows = 3
+//Output: "PAHNAPLSIIGYIR"
+//Example 2:
+//
+//Input: s = "PAYPALISHIRING", numRows = 4
+//Output: "PINALSIGYAHRPI"
+//Explanation:
+//P     I    N
+//A   L S  I G
+//Y A   H R
+//P     I
+//Example 3:
+//
+//Input: s = "A", numRows = 1
+//Output: "A"
+//
+//
+//Constraints:
+//
+//        1 <= s.length <= 1000
+//s consists of English letters (lower-case and upper-case), ',' and '.'.
+//        1 <= numRows <= 1000
+
+public class Zigzag {
+
+
+    public  static void  main(String [] args)
+    {
+        System.out.println(convert("PAYPALISHIRING",4));
+    }
+
+    public static String convert(String s, int numRows) {
+
+        if (numRows == 1) return s;
+
+        StringBuilder[] values = new StringBuilder[numRows];
+        for (int i = 0; i < numRows; i++)
+            values[i] = new StringBuilder();
+        int step=0;
+        boolean down = true;
+
+        for(int i=0;i<s.length();i++)
+        {
+                values[step].append(s.charAt(i));
+
+            if (down) {
+                step++;
+                if (step == numRows - 1) down = false;
+            } else {
+                step--;
+                if (step == 0) down = true;
+            }
+
+        }
+
+
+        StringBuilder result = new StringBuilder();
+        for (StringBuilder sb : values)
+            result.append(sb);
+
+        return result.toString();
+    }
+}
+
+
